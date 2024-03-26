9b8f545d058b5f8bf1caa538099a08b50eebde90
ianshih <ianshih2003@gmail.com>
2024-03-26 03:27:20 -0300

    EJ 8

--- server/common/server.py
+++ server/common/server.py
@@ -1,13 +1,8 @@
 import socket
 import logging
 import signal
-from common.utils import load_bets, process_bets, get_winner_bets_by_agency
-
-MAX_MESSAGE_BYTES = 4
-EXIT = "exit"
-WINNERS = "winners"
-CONFIRMATION_MSG_LENGTH = 3
-SUCCESS_MSG = "suc"
+from multiprocessing import Process, Lock, Value
+from common.client_handler import create_client_handler
 
 
 class Server:
@@ -17,11 +12,14 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
-        self.client_sock = None
 
         self.clients = clients
 
-        self.finished_clients = 0
+        self.finished_clients = Value('i', 0)
+
+        self.file_lock = Lock()
+
+        self.processes = []
 
     def run(self):
         """
@@ -34,65 +32,16 @@ class Server:
         while True:
             try:
                 client_sock = self.__accept_new_connection()
-                self.client_sock = client_sock
-                self.__handle_client_connection()
+
+                process = Process(target=create_client_handler,
+                                  args=(client_sock, self.file_lock, self.finished_clients, self.clients))
+
+                self.processes.append(process)
+                process.start()
             except OSError:
                 break
-
-    def __receive_message_length(self):
-        try:
-
-            msg_length = int.from_bytes(self.__safe_receive(
-                MAX_MESSAGE_BYTES).rstrip(), "little")
-
-            logging.info(
-                f"action: receive_message_length | result: success | length: {msg_length}")
-
-            self.__send_success_message()
-
-            return msg_length
-        except socket.error as e:
-            logging.error(
-                f"action: receive_message_length | result: failed | error: client disconnected")
-            raise e
-        except Exception as e:
-            self.__send_error_message()
-            logging.error(
-                f"action: receive_message_length | result: failed | error: {e}")
-            return 0
-
-    def __handle_client_connection(self):
-        """
-        Read message from a specific client socket and closes the socket
-
-        If a problem arises in the communication with the client, the
-        client socket will also be closed
-        """
-        while True:
-            try:
-                msg_length = self.__receive_message_length()
-
-                if msg_length == 0:
-                    return
-
-                msg = self.__safe_receive(msg_length).rstrip()
-                self.__log_ip()
-
-                self.__process_message(msg)
-
-            except OSError as e:
-                self.__send_error_message()
-                logging.error(
-                    f"action: receive_message | result: fail | error: {e}")
-                break
-            except:
-                break
-        self._close_client_socket()
-
-    def __log_ip(self):
-        addr = self.client_sock.getpeername()
-        logging.info(
-            f'action: receive_message | result: success | ip: {addr[0]}')
+        for process in self.processes:
+            process.join()
 
     def __accept_new_connection(self):
         """
@@ -117,85 +66,8 @@ class Server:
         self._server_socket.close()
         logging.info('action: closing listening socket | result: success')
 
-        self._close_client_socket()
+        for process in self.processes:
+            process.terminate()
 
         logging.info(
             f'action: receive_termination_signal | result: success')
-
-    def _close_client_socket(self):
-        logging.info('action: closing client socket | result: in_progress')
-        if self.client_sock:
-
-            self.finished_clients += 1
-
-            self.client_sock.close()
-
-            self.client_sock = None
-        logging.info('action: closing client socket | result: success')
-
-    def __send_success_message(self):
-        self.__safe_send("suc".encode())
-        logging.info('action: send sucess message | result: success')
-
-    def __send_error_message(self):
-        self.__safe_send("err".encode())
-        logging.error('action: send error message | result: success')
-
-    def __safe_send(self, message: bytes):
-        n = 0
-        max_tries = 5
-
-        while n != len(message) and max_tries > 0:
-            n = self.client_sock.send(message)
-            max_tries -= 1
-        return
-
-    def __safe_receive(self, buffer_length: int):
-        n = 0
-
-        buffer = bytes()
-        while n < buffer_length:
-            message = self.client_sock.recv(buffer_length)
-            buffer += message
-            n += len(message)
-
-        return buffer
-
-    def __process_message(self, message: bytes):
-        msg = message.decode()
-
-        split_msg = msg.split(",")
-        logging.info(split_msg)
-        if msg == EXIT:
-            raise socket.error("Client disconnected")
-        elif len(split_msg) == 2 and split_msg[0] == WINNERS:
-            self.__send_winners(split_msg[1])
-        else:
-            process_bets(msg)
-            self.__send_success_message()
-
-    def __send_winners(self, agency: str):
-        if self.finished_clients < self.clients:
-            self.__send("waiting".encode())
-            return
-
-        bets = load_bets()
-
-        winner_bets = get_winner_bets_by_agency(bets, agency)
-
-        dnis = map(lambda bet: bet.document, winner_bets)
-
-        response = ",".join(dnis)
-
-        self.__send(response.encode())
-
-    def __send(self, message: bytes):
-        self.__safe_send(len(message).to_bytes(MAX_MESSAGE_BYTES, 'little'))
-
-        if self.__safe_receive(CONFIRMATION_MSG_LENGTH).decode() != SUCCESS_MSG:
-            raise socket.error("rejected")
-
-        self.__safe_send(message)
-
-        if self.__safe_receive(CONFIRMATION_MSG_LENGTH).decode() != SUCCESS_MSG:
-            raise socket.error("rejected")
